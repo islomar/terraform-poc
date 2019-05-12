c100be2395d5b3a4884cc461a802df4a3f908849
Isidro López <islomar@gmail.com>
2019-05-12 22:44:24 +0200

    Add Elastic IP resource

--- example.tf
+++ example.tf
@@ -3,6 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
+}
+
+# Assign an Elastic IP to the EC2 instance
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
 }
