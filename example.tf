66840493a7af1fe13c5b7377d80ca2364ac53bdc
Isidro Lopez <isidro.lopez@form3.tech>
2019-05-13 16:24:51 +0200

    Add local-exec provisioner

--- example.tf
+++ example.tf
@@ -17,6 +17,10 @@ resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+
   # Tells Terraform that this EC2 instance must be created only after the
   # S3 bucket has been created.
 #  depends_on = ["aws_s3_bucket.example"]
